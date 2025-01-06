165628fed69aa7c82448a3e8686e133c1ad3872e
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2025-01-05 16:48:42 -0800

    udate
    update

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Staff, Roster
 
-# Create your views here.
+def staff_list(request):
+    active_staff = Staff.objects.filter(is_active=True)  # Get only active staff
+    return render(request, 'roster/staff_list.html', {'staff_list': active_staff})
+
+def roster_create(request):
+    active_staff = Staff.objects.filter(is_active=True)  # Get active staff
+    if request.method == 'POST':
+        for staff in active_staff:
+            for day in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']:
+                time_choice = request.POST.get(f"{staff.id}_{day}")
+                if time_choice:
+                    if time_choice == 'AM':
+                        Roster.objects.create(staff=staff, day=day, shift_start='09:00:00', shift_end='14:00:00')
+                    elif time_choice == 'PM':
+                        Roster.objects.create(staff=staff, day=day, shift_start='14:00:00', shift_end='19:00:00')
+                    elif time_choice == 'FULL':
+                        Roster.objects.create(staff=staff, day=day, shift_start='09:00:00', shift_end='19:00:00')
+        return redirect('roster_list')
+    return render(request, 'roster/roster_create.html', {'staff_list': active_staff})
+
+def roster_list(request):
+    rosters = Roster.objects.all()
+    return render(request, 'roster/roster_list.html', {'rosters': rosters})
+
