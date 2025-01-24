e932726bce08625a8dfe59cd54975b5e22d36de6
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2025-01-23 18:33:27 -0800

    update

--- roster/templatetags/custom_filters.py
+++ roster/templatetags/custom_filters.py
@@ -1,8 +1,9 @@
 from django import template
+from ..models import Roster
 
 register = template.Library()
 
 @register.filter
-def get_roster(rosters, staff_id, day):
-    # 实现你的逻辑
-    return next((r for r in rosters if r.staff.id == staff_id and r.day == day), None)
+def get_roster(rosters, staff_id_day):
+    staff_id, day = staff_id_day.split('_')
+    return next((r for r in rosters if r.staff.id == int(staff_id) and r.day == day), None)
