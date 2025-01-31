071564e0c13b0e98e045837c2bdb038e0c2c1ffa
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2025-01-30 18:59:53 -0800

    home

--- bakery_roster/urls.py
+++ bakery_roster/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),  # Add this line for the home view
+    path('home/', views.home, name='home_redirect'),  # Home view at /home
     path('admin/', admin.site.urls),       # Admin interface URL
     path('roster/', include('roster.urls')),  # Ensure this points to your roster app
     # Remove the following line to avoid redundancy
